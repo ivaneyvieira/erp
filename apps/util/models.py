adc6b8ba38d3a2853d48a1f349eafb0158d6ede2
Ivaney <ivaneyvieira@gmail.com>
2017-01-25 20:55:33 -0300

    comit

--- apps/util/models.py
+++ apps/util/models.py
@@ -1,15 +1,24 @@
 from django.db import models
 
-# Create your models here.
+
 class Regiao(models.Model):
     nome = models.CharField(max_length=20)
     sigla = models.CharField(max_length=2, unique=True)
+
     def __str__(self):
         return "{sg} - {nm}".format(sg=self.sigla, nm=self.nome)
+
 
 class Estado(models.Model):
     nome = models.CharField(max_length=20)
     sigla = models.CharField(max_length=2, unique=True)
     regiao = models.ForeignKey(Regiao, on_delete=models.CASCADE)
+
     def __str__(self):
         return "{sg} - {nm}".format(sg=self.sigla, nm=self.nome)
+
+
+class Cidade(models.Model):
+    nome = models.CharField(max_length=32)
+    estado = models.ForeignKey(Estado, on_delete=models.CASCADE)
+    ddd = models.CharField(max_length=3)
