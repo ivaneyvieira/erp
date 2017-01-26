04f78fc6c20cab27476c8494d5aca391f482123d
Ivaney <ivaneyvieira@gmail.com>
2017-01-25 21:04:01 -0300

    commit 2

--- apps/util/admin.py
+++ apps/util/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from . models import Regiao, Estado
+from . models import Estado, Regiao, Cidade
 # Register your models here.
 
 
 admin.site.register(Regiao)
 admin.site.register(Estado)
+admin.site.register(Cidade)
